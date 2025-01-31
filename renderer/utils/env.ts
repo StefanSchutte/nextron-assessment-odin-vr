143cf524f67ca2cc116aeb79e4deb926014ef5c1
Stefan Schutte <2011070287@ufs4life.ac.za>
2025-01-31 07:36:17 +0200

    public-next

--- renderer/utils/env.ts
+++ renderer/utils/env.ts
@@ -1,7 +1,7 @@
 export const loadEnvConfig = () => {
     return {
-        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || '',
-        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || '',
+        AWS_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID || '',
+        AWS_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY || '',
         AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION || '',
         AWS_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME || '',
         // Cognito Configuration
